a53f09f8013407e3364bbe5987b02fa94fd9edfa
manuelzach <zach@crowdee.com>
2024-08-19 14:19:35 +0200

    added script for prettier

--- css-modules.d.ts
+++ css-modules.d.ts
@@ -1,5 +1,5 @@
 // css-modules.d.ts
-declare module '*.module.css' {
-    const classes: { [key: string]: string };
-    export default classes;
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  export default classes;
 }
