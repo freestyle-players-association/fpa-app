8fd5e867a39f17ab6ba084bdaab8106c7531d967
manuelzach <zach@crowdee.com>
2024-09-06 14:27:04 +0200

    added formatting for time and date

--- components/event/details/event-details.tsx
+++ components/event/details/event-details.tsx
@@ -1,20 +1,18 @@
 import { Tables } from "@/utils/supabase/database.types";
 import { Card } from "@/components/common/card";
+import { formatDate } from "@/utils/formatting/date-time-formatting";
 
 type EventDetailsProps = {
   event: Tables<"events">;
 };
 
 export default async function EventDetails({ event }: EventDetailsProps) {
-  const start_date = new Date(event.start_date ?? "").toLocaleDateString();
-  const end_date = new Date(event.end_date ?? "").toLocaleDateString();
-
   return (
     <Card className="p-4 mb-4">
       <h2>{event.name}</h2>
       <p>{event.description}</p>
       <p>
-        {start_date} - {end_date}
+        {formatDate(event.start_date)} - {formatDate(event.end_date)}
       </p>
     </Card>
   );
