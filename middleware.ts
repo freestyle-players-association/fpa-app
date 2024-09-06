edf1b23291f45cc90a9eeac956739a741e5199ac
manuelzach <zach@crowdee.com>
2024-09-06 17:11:51 +0200

    redirect to longin for unauthenticated users on protectedRoutes

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,34 @@
 import createMiddleware from "next-intl/middleware";
-import { type NextRequest } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 import { updateSession } from "./utils/supabase/middleware";
 import { routing } from "@/i18n/routing";
+import { cookies } from "next/headers";
 
 const handleI18nRouting = createMiddleware(routing);
+
+const protectedRoutes = ["/user-profile", "/tournaments/create"];
 
 export async function middleware(request: NextRequest) {
   const response = handleI18nRouting(request);
 
-  // A `response` can now be passed here
+  const path = request.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.some((protectedRoute) =>
+    path.includes(protectedRoute),
+  );
+
+  // I don't know if I find this fishy yet. Did not want to sign the session cookie my self.
+  // If we need to look into authorization for redirection as well,
+  // then we need to sign auth cookie our self
+  const hasSessionCookie = Boolean(
+    cookies()
+      .getAll()
+      .find((cookie) => cookie.name.includes("auth-token")),
+  );
+
+  if (isProtectedRoute && !hasSessionCookie) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+
   return await updateSession(request, response);
 }
 
--- next-server-functions/user/auth-actions.ts
+++ next-server-functions/user/auth-actions.ts
@@ -52,4 +52,5 @@ export const signOut = async () => {
   const supabase = createClient();
   await supabase.auth.signOut();
   await supabase.auth.refreshSession();
+  redirect("/login");
 };
