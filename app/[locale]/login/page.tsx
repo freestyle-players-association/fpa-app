824962941de932aabdd280bbdbf56e8fc7027540
fluid-movement <derandre3000@hotmail.com>
2024-09-07 19:31:03 +0200

    commit so i can switch machines

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -4,6 +4,8 @@ import { Input } from "@/components/common/input";
 import { Label } from "@/components/common/label";
 import { buttonVariants } from "@/components/common/button";
 import { Link } from "@/i18n/routing";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Login({
   searchParams,
