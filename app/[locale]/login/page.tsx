88be5865761ae78b2a07157748af1416603fe6b7
fluid-movement <derandre3000@hotmail.com>
2024-09-07 19:31:03 +0200

    commit so i can switch machines

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -1,6 +1,8 @@
 import { SubmitButton } from "./submit-button";
 import { signIn } from "@/next-server-functions/user/auth-actions";
 import { Link } from "@/i18n/routing";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Login({
   searchParams,
@@ -9,52 +11,27 @@ export default function Login({
 }) {
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      <Link
-        href="/"
-        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-        >
-          <polyline points="15 18 9 12 15 6" />
-        </svg>{" "}
-        Back
-      </Link>
-
       <form className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
-        <label className="text-md" htmlFor="email">
+        <Label className="text-md" htmlFor="email">
           Email
-        </label>
-        <input
+        </Label>
+        <Input
           className="rounded-md px-4 py-2 bg-inherit border mb-6"
           name="email"
           placeholder="you@example.com"
           required
         />
-        <label className="text-md" htmlFor="password">
+        <Label className="text-md" htmlFor="password">
           Password
-        </label>
-        <input
+        </Label>
+        <Input
           className="rounded-md px-4 py-2 bg-inherit border mb-6"
           type="password"
           name="password"
           placeholder="••••••••"
           required
         />
-        <SubmitButton
-          formAction={signIn}
-          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing In..."
-        >
+        <SubmitButton formAction={signIn} pendingText="Signing In...">
           Sign In
         </SubmitButton>
         <Link href={"/sign-up"}>Sign Up</Link>
