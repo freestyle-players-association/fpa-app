60ece43672182853a0c5bf933681f982f95854f0
manuelzach <zach@devconsultberlin.de>
2024-09-09 13:24:15 +0200

    added custom eslint rule for permitting hooks in async components

--- app/[locale]/user-profile/update/page.tsx
+++ app/[locale]/user-profile/update/page.tsx
@@ -2,9 +2,9 @@ import UpdateUserProfileAvatarForm from "@/components/user-profile/form/update-u
 import UpdateUserProfileForm from "@/components/user-profile/form/update-profile-form/udate-profile-form";
 import { getAuthenticatedUserProfile } from "@/next-server-functions/user-profile/user-profile-data";
 import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
-import { Card } from "@/components/common/card";
-import { Separator } from "@/components/common/separator";
+import { Card } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { redirect } from "@/i18n/routing";
 
 export default async function UpdateUserProfilePage() {
   const supabase = createClient();
@@ -19,8 +19,8 @@ export default async function UpdateUserProfilePage() {
       <h3>Avatar</h3>
       <UpdateUserProfileAvatarForm
         initialValues={{
-          username: userProfile.username,
-          avatar_url: userProfile.avatar_url,
+          username: userProfile!.username,
+          avatar_url: userProfile!.avatar_url,
         }}
       />
       <Separator className="my-4" />
