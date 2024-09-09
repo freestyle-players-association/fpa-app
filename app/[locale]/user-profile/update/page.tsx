18568dff2a97ebbc0681c927fefd17f0067ee85f
manuelzach <zach@devconsultberlin.de>
2024-09-09 16:04:16 +0200

    adjustments after rebase

--- app/[locale]/user-profile/update/page.tsx
+++ app/[locale]/user-profile/update/page.tsx
@@ -2,8 +2,8 @@ import UpdateUserProfileAvatarForm from "@/components/user-profile/form/update-u
 import UpdateUserProfileForm from "@/components/user-profile/form/update-profile-form/udate-profile-form";
 import { getAuthenticatedUserProfile } from "@/next-server-functions/user-profile/user-profile-data";
 import { createClient } from "@/utils/supabase/server";
-import { Card } from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
+import { Card } from "@/components/common/card";
+import { Separator } from "@/components/common/separator";
 import { redirect } from "@/i18n/routing";
 
 export default async function UpdateUserProfilePage() {
