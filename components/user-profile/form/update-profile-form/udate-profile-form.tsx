748d6975d77940d42553e1ca9b799ffa4ef55fd1
Mati <58902384+mat8900@users.noreply.github.com>
2024-09-16 17:06:38 +0200

    Add update profile  first name, last name, date of birth fields (#40)

--- components/user-profile/form/update-profile-form/udate-profile-form.tsx
+++ components/user-profile/form/update-profile-form/udate-profile-form.tsx
@@ -21,16 +21,32 @@ export default function UpdateUserProfileForm({
       action={dispatch}
       style={{ display: "flex", flexDirection: "column", gap: "1rem" }}
     >
-      <Label>
-        <Input
-          name="username"
-          type="text"
-          required
-          placeholder="Username"
-          defaultValue={initialValues.username}
-        />
-      </Label>
+      <Label htmlFor="username">First Name</Label>
+      <Input
+        name="username"
+        type="text"
+        required
+        placeholder="Username"
+        defaultValue={initialValues.username}
+      />
 
+      <Label htmlFor="last_name">Last Name</Label>
+      <Input
+        name="last_name"
+        type="text"
+        required
+        placeholder="Last Name"
+        defaultValue={initialValues.last_name || ""}
+      />
+
+      <Label htmlFor="date_of_birth">Date of birth</Label>
+      <Input
+        name="date_of_birth"
+        type="date"
+        required
+        placeholder="date of birth"
+        defaultValue={initialValues.date_of_birth || ""}
+      />
       <FormSubmitButton>Update user Profile</FormSubmitButton>
     </form>
   );
